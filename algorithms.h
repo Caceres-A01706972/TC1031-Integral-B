7c41d036661f86a8d54f2757ebc4cd409cac7f67
Ricardo Cáceres <83652905+Caceres-A01706972@users.noreply.github.com>
2021-11-18 10:46:30 -0600

    Agrega archivo con algoritmos

---
+++ algorithms.h
@@ -0,0 +1,124 @@
+
+#include <string>
+#include <stdio.h>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <list>
+#include <algorithm>
+
+#include "validation.h"
+
+using namespace std;
+
+
+class Graph {
+private:
+		int edgesList;
+		int edgesMat;
+		int nodes;
+    	vector<int> *adjList;
+		int *adjMatrix;
+		//vector<Type> *vect = new vector<Type>
+
+public:
+		void loadGraphMat(string, int, int);
+		Graph(int);
+		Graph();
+		void addEdgeAdjMatrix(int, int);
+		string printAdjMat();
+		string printAdjMat_clean();
+		bool contains(list<int>, int);
+		void sortAdjList();
+
+        string printAdjList();
+};
+
+
+void Graph::loadGraphMat(string name, int a, int b){
+	adjMatrix = new int [a*b];
+	nodes = a;
+	for (int i = 0; i < a*b; i++)
+		adjMatrix[i] = 0;
+		string line;
+		ifstream lee (name);
+		int u, v;
+		if (lee.is_open()){
+			while (getline(lee, line)){
+				u = stoi(line.substr(1,1));
+				v = stoi(line.substr(4,1));
+				addEdgeAdjMatrix(u,v);
+			}
+			lee.close(); // Closes the file
+		} else {
+			cout << "Unable to open file";
+		}
+}
+
+Graph::Graph() {
+	edgesList = edgesMat = 0;
+}
+
+Graph::Graph(int n) {
+	nodes = n;
+	adjList = new vector<int>[nodes];
+	adjMatrix = new int [nodes*nodes];
+	for (int i = 0; i < nodes*nodes; i++)
+		adjMatrix[i] = 0;
+	edgesList = edgesMat = 0;
+}
+
+void Graph::addEdgeAdjMatrix(int u, int v){
+	adjMatrix[u*nodes+v] = 1;
+	adjMatrix[v*nodes+u] = 1;
+	edgesMat++;
+    //cout<<"\n Printing the Connections\n";
+}
+
+string Graph::printAdjList(){
+	  stringstream aux;
+		for (int i = 0; i < nodes; i++){
+	        aux << "vertex "
+	             << i << " :";
+	        for (int j = 0; j < adjList[i].size(); j ++){
+							 aux << " " << adjList[i][j];
+					}
+	        aux << " ";
+    }
+		return aux.str();
+
+}
+
+string Graph::printAdjMat(){
+	stringstream aux;
+	for (int i = 0; i < nodes; i++){
+	   for (int j = 0; j < nodes; j++){
+			 aux << adjMatrix[i*nodes+j] << " ";
+		 }
+  }
+	return aux.str();
+}
+
+string Graph::printAdjMat_clean(){
+	stringstream aux;
+	aux << "\n Fights |";
+	for (int i = 0; i < nodes; i++){
+			aux << "\t" << validation(i) ;
+	}
+	aux << "\n";
+	for (int i = 0; i < nodes; i++){
+			aux << "__________";
+	}
+	aux << "\n";
+	for (int i = 0; i < nodes; i++){
+		 aux << validation(i) << "\t|";
+	   for (int j = 0; j < nodes; j++){
+			 aux << "\t" << adjMatrix[i*nodes+j];
+		 }
+	   aux << "\n";
+  }
+	return aux.str();
+}
