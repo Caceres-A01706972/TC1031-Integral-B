8fdbb0776ee37723cda28fccd62e30b701a3fbdb
Ricardo Cáceres <83652905+Caceres-A01706972@users.noreply.github.com>
2021-12-01 09:36:51 -0600

    Se agrego el uso de hashes

--- main.cpp
+++ main.cpp
@@ -8,13 +8,17 @@
 #include <stack>
 #include <list>
 #include <algorithm>
+#include <iterator>
+#include <set>
 
 #include "algorithms.h"
 #include "hash.h"
 #include "reverse_validation.h"
+#include "sets.h"
 
 using namespace std;
 
+//Mi funcion hash 
 unsigned int myHash(const string s) {
 	unsigned int acum = 0;
 	for (unsigned int i = 0; i < s.size(); i++) {
@@ -27,12 +31,25 @@ int main(){
 
     bool continua = true;
     int resp;
+    int asiento;
     Graph g;
     g.loadGraphMat("rutas.txt", 10, 10);
     g.loadGraphList("rutas.txt", 10, 10);
     
     HashTable<string, int> hash(20, string("empty"), myHash);
-
+    set<int, greater<int> > seats_disponibles;
+    create_seats(seats_disponibles);
+    /*
+    seats_disponibles.insert(2);
+    seats_disponibles.insert(19);
+    seats_disponibles.insert(9);
+    seats_disponibles.insert(23);
+    seats_disponibles.insert(21);
+    seats_disponibles.insert(17);
+    seats_disponibles.insert(24);
+    seats_disponibles.insert(8);
+    seats_disponibles.insert(15);
+*/
    
     while(continua){
         string nombre;
@@ -41,73 +58,77 @@ int main(){
         string ruta;
         int pasaporte;
 
-        cout<<"\nMENU"<<endl;
-        cout<<"=========\n";
-        cout<<"1. Ver lista de Vuelos"<<endl;
-        cout<<"2. Buscar Ruta Aventurera"<<endl;
-        cout<<"3. Buscar Ruta Rapida"<<endl;
-        cout<<"4. Comprar Boleto"<<endl;
-        cout<<"5. Opcion Exclusiva para Administrador"<<endl;
-        cout<<"6. Exit"<<endl;
+        std::cout<<"\nMENU"<<endl;
+        std::cout<<"=========\n";
+        std::cout<<"1. Ver lista de Vuelos"<<endl;
+        std::cout<<"2. Buscar Ruta Aventurera"<<endl;
+        std::cout<<"3. Buscar Ruta Rapida"<<endl;
+        std::cout<<"4. Comprar Boleto"<<endl;
+        std::cout<<"5. Opcion Exclusiva para Administrador"<<endl;
+        std::cout<<"6. Exit"<<endl;
         cin>>resp;
 
         if (resp == 1){
-            cout<<g.printAdjMat_clean();
+            std::cout<<g.printAdjMat_clean();
 
         } else if (resp == 2){
-            cout<<"Ingesa el pais de donde sales (ejemplo: MEX):  "<<endl;
+            std::cout<<"Ingesa el pais de donde sales (ejemplo: MEX):  "<<endl;
             cin>>start;
-            cout<<"Ingresa el pais de destino (ejemplo: ESA): "<<endl;
+            std::cout<<"Ingresa el pais de destino (ejemplo: ESA): "<<endl;
             cin>>destino;
-            cout<<g.DFS(reverse_validation(start), reverse_validation(destino));
+            std::cout<<g.DFS(reverse_validation(start), reverse_validation(destino));
 
         } else if (resp == 3){
-            cout<<"Ingesa el pais de donde sales (ejemplo: MEX):  "<<endl;
+            std::cout<<"Ingesa el pais de donde sales (ejemplo: MEX):  "<<endl;
             cin>>start;
-            cout<<"Ingresa el pais de destino (ejemplo: ESA): "<<endl;
+            std::cout<<"Ingresa el pais de destino (ejemplo: ESA): "<<endl;
             cin>>destino;
             g.printShortestDistance(reverse_validation(start), reverse_validation(destino), 10);
 
         } else if (resp == 4){
-            cout<<"Ingresa el nombre del pasajero: "<<endl;
+            std::cout<<"Ingresa el nombre del pasajero: "<<endl;
             cin>>nombre;
-            cout<<"Ingesa el pais de donde sales (ejemplo: MEX):  "<<endl;
+            std::cout<<"Ingesa el pais de donde sales (ejemplo: MEX):  "<<endl;
             cin>>start;
-            cout<<"Ingresa el pais de destino (ejemplo: ESA): "<<endl;
+            std::cout<<"Ingresa el pais de destino (ejemplo: ESA): "<<endl;
             cin>>destino;
-            cout<<"Desea la Ruta Rapida (1) o la Ruta Aventurera (2)"<<endl;
-            cin>>resp;
-            cout<<"Ingresa tu numero de pasaporte: "<<endl;
+            std::cout<<"Elige el asiento que deseas comprar: "<<endl;
+            print_asientos(seats_disponibles);
+            cin>>asiento;
+            ocupa_asiento(seats_disponibles, asiento);
+            std::cout<<"Ingresa tu numero de pasaporte: "<<endl;
             cin>>pasaporte;
+            std::cout<<"Desea la Ruta Rapida (1) o la Ruta Aventurera (2)"<<endl;
+            cin>>resp;
             hash.put(string(nombre), pasaporte);
             if (resp == 1){
                 ruta = "Rapida";
-                cout<<"La Ruta de tu viaje sera la siguiente: "<<endl;
+                std::cout<<"La Ruta de tu viaje sera la siguiente: "<<endl;
                 g.printShortestDistance(reverse_validation(start), reverse_validation(destino), 10);
             } else if (resp == 2){
                 ruta = "Aventurera";
-                cout<<g.DFS(reverse_validation(start), reverse_validation(destino));
+                std::cout<<g.DFS(reverse_validation(start), reverse_validation(destino));
             }
             write_to_file(nombre, start, destino, ruta);
-            cout<<"\nPasaje Comprado Exitosamente"<<endl;
+            std::cout<<"\nPasaje Comprado Exitosamente"<<endl;
 
         } else if (resp == 5){
-            cout<<"Para esta opcion debes ser un administrador, ingresa contrasenia de administrador (contrasenia: 123456): "<<endl;
+            std::cout<<"Para esta opcion debes ser un administrador, ingresa contrasenia de administrador (contrasenia: 123456): "<<endl;
             cin>> resp;
             if (resp == 123456){
-                cout<<"\nMenu de Administrador"<<endl;
-                cout<<"1. Ver Pasaporte de pasajero"<<endl;
-                cout<<"2. Ver Tabla de Pasajeros y sus Pasaportes"<<endl;
+                std::cout<<"\nMenu de Administrador"<<endl;
+                std::cout<<"1. Ver Pasaporte de pasajero"<<endl;
+                std::cout<<"2. Ver Tabla de Pasajeros y sus Pasaportes"<<endl;
                 cin>> resp;
                 if(resp == 1){
-                    cout<<"Ingresa el nombre del pasajero: "<<endl;
+                    std::cout<<"Ingresa el nombre del pasajero: "<<endl;
                     cin>> nombre;
-                    cout<<hash.get(nombre);
+                    std::cout<<hash.get(nombre);
                 } else if (resp == 2){
-                    cout<<hash.toString();
+                    std::cout<<hash.toString();
                 }
             } else if (resp != 123456){
-                cout<<"Contrasenia Incorrecta";
+                std::cout<<"Contrasenia Incorrecta";
             }
         } else if (resp == 6){
             break;
